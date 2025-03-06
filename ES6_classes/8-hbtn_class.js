a1677f13939153d1c28174e6dbfe4490e6f932b8
NatalieR89 <natalieritch89@gmail.com>
2025-03-05 23:03:25 -0600

    linter fixes

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -1,21 +1,21 @@
 export default class HolbertonClass {
-constructor(size, location) {
-if (typeof size !== 'number') {
-throw new TypeError('size must be a number');
-}
-if (typeof location !== 'string') {
-throw new TypeError('location must be a string');
-}
+  constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw new TypeError('size must be a number');
+    }
+    if (typeof location !== 'string') {
+      throw new TypeError('location must be a string');
+    }
 
-this._size = size;
-this._location = location;
-}
+    this._size = size;
+    this._location = location;
+  }
 
-valueOf() {
-return this._size;
-}
+  valueOf() {
+    return this._size;
+  }
 
-toString() {
-return this._location;
-}
+  toString() {
+    return this._location;
+  }
 }
