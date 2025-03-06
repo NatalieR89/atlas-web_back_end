23788f590f8222196dc6380dd3ae914e92b43353
NatalieR89 <natalieritch89@gmail.com>
2025-03-05 19:55:53 -0600

    im sure its the linter.

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,22 @@
+export default class Building {
+    constructor(sqft) {
+      if (new.target === Building) {
+        throw new Error("Cannot instantiate an abstract class");
+      }
+  
+      if (typeof sqft !== 'number') {
+        throw new TypeError("sqft must be a number");
+      }
+  
+      this._sqft = sqft;
+    }
+  
+    get sqft() {
+      return this._sqft;
+    }
+  
+    evacuationWarningMessage() {
+      throw new Error("Class extending Building must override evacuationWarningMessage");
+    }
+  }
+  
