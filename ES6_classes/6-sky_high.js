03e626b470998593198cc48fce4050c91808a9a6
NatalieR89 <natalieritch89@gmail.com>
2025-03-05 23:48:05 -0600

    linter fixes

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,13 +1,13 @@
-import Building from "./5-building.js";
+import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    
+
     if (typeof floors !== 'number') {
-      throw new TypeError("floors must be a number");
+      throw new TypeError('floors must be a number');
     }
-    
+
     this._floors = floors;
   }
 
