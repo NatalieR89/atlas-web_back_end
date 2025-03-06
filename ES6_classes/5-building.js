74c6f97574f59997a137b31f2afc1a27c51dfac7
NatalieR89 <natalieritch89@gmail.com>
2025-03-05 23:36:12 -0600

    linter fixes

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,19 +1,20 @@
 class Building {
-constructor(sqft) {
-this._sqft = sqft;
+  constructor(sqft) {
+    this._sqft = sqft;
 
-if (this.constructor !== Building && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
-throw new Error("Class extending Building must override evacuationWarningMessage");
+if (this.constructor !== Building && this.evacuationWarningMessage === 
+  Building.prototype.evacuationWarningMessage) {
+    throw new Error("Class extending Building must override evacuationWarningMessage");
 }
-}
-
-get sqft() {
-return this._sqft;
 }
 
-evacuationWarningMessage() {
-throw new Error("Class extending Building must override evacuationWarningMessage");
-}
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error("Class extending Building must override evacuationWarningMessage");
+  }
 }
 
 export default Building;
