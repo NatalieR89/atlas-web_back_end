92481ac39efe16c1c8426920c5e6e3f82a96ec98
NatalieR89 <natalieritch89@gmail.com>
2025-03-05 23:33:45 -0600

    idfk

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,9 +1,10 @@
 class Building {
 constructor(sqft) {
-if (new.target === Building) {
-throw new Error("Building is an abstract class and cannot be instantiated directly");
-}
 this._sqft = sqft;
+
+if (this.constructor !== Building && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+throw new Error("Class extending Building must override evacuationWarningMessage");
+}
 }
 
 get sqft() {
