30146a00e24f60173609db2942de1366091fb030
NatalieR89 <natalieritch89@gmail.com>
2025-03-05 22:57:50 -0600

    linter fixes

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,17 +1,17 @@
 export default class Airport {
-    constructor(name, code) {
-      if (typeof name !== 'string') {
-        throw new TypeError('name must be a string');
-      }
-      if (typeof code !== 'string') {
-        throw new TypeError('code must be a string');
-      }
-
-      this._name = name;
-      this._code = code;
+  constructor(name, code) {
+    if (typeof name !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+    if (typeof code !== 'string') {
+      throw new TypeError('code must be a string');
     }
 
-    toString() {
-      return `[object ${this._code}]`;
-    }
+    this._name = name;
+    this._code = code;
   }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
