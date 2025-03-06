db7c2f938147d7d12d823760ab99d17e6b6dc0e7
NatalieR89 <natalieritch89@gmail.com>
2025-03-05 23:30:57 -0600

    5 fixes

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,22 +1,18 @@
-export default class Building {
-    constructor(sqft) {
-      if (new.target === Building) {
-        throw new Error("Cannot instantiate an abstract class");
-      }
-  
-      if (typeof sqft !== 'number') {
-        throw new TypeError("sqft must be a number");
-      }
-  
-      this._sqft = sqft;
-    }
-  
-    get sqft() {
-      return this._sqft;
-    }
-  
-    evacuationWarningMessage() {
-      throw new Error("Class extending Building must override evacuationWarningMessage");
-    }
-  }
-  
+class Building {
+constructor(sqft) {
+if (new.target === Building) {
+throw new Error("Building is an abstract class and cannot be instantiated directly");
+}
+this._sqft = sqft;
+}
+
+get sqft() {
+return this._sqft;
+}
+
+evacuationWarningMessage() {
+throw new Error("Class extending Building must override evacuationWarningMessage");
+}
+}
+
+export default Building;
