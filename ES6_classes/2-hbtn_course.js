67faad466e0490a89c0186fc81d762ec1d22511f
NatalieR89 <natalieritch89@gmail.com>
2025-03-05 19:45:52 -0600

    lint fixes

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,7 +1,7 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') {
-        throw new TypeError("Name must be a string");
+      throw new TypeError("Name must be a string");
     }
     if (typeof length !== 'number') {
       throw new TypeError("Length must be a number");
@@ -9,7 +9,7 @@ export default class HolbertonCourse {
     if (!Array.isArray(students) || !students.every(student => typeof student === 'string')) {
       throw new TypeError("Students must be an array of strings");
     }
-  
+
     this._name = name;
     this._length = length;
     this._students = students;
@@ -36,16 +36,15 @@ export default class HolbertonCourse {
     }
     this._length = value;
   }
-  
+
   get students() {
     return this._students;
   }
 
-    set students(value) {
-      if (!Array.isArray(value) || !value.every(student => typeof student === 'string')) {
-        throw new TypeError("Students must be an array of strings");
-      }
-      this._students = value;
+  set students(value) {
+    if (!Array.isArray(value) || !value.every(student => typeof student === 'string')) {
+      throw new TypeError("Students must be an array of strings");
     }
+    this._students = value;
   }
-  
+}
