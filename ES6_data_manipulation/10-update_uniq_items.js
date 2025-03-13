0dc27043a6b9fc8f656977afffcd8d6b766af5a9
NatalieR89 <natalieritch89@gmail.com>
2025-03-12 21:01:42 -0500

    lint ready

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,13 @@
+function updateUniqueItems(groceriesMap) {
+  if (!(groceriesMap instanceof Map)) {
+    throw new Error('Cannot process');
+}
+
+for (let [key, value] of groceriesMap) {
+  if (value === 1) {
+    groceriesMap.set(key, 100);
+    }
+  }
+}
+
+export default updateUniqueItems;
