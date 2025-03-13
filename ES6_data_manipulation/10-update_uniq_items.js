6ba1e865ac3feba16f6f1d663c05d7acfccaddde
NatalieR89 <natalieritch89@gmail.com>
2025-03-12 21:14:15 -0500

    lint fixes

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,11 +1,11 @@
 function updateUniqueItems(groceriesMap) {
   if (!(groceriesMap instanceof Map)) {
     throw new Error('Cannot process');
-}
+  }
 
-for (let [key, value] of groceriesMap) {
-  if (value === 1) {
-    groceriesMap.set(key, 100);
+  for (let [key, value] of groceriesMap) {
+    if (value === 1) {
+      groceriesMap.set(key, 100);
     }
   }
 }
