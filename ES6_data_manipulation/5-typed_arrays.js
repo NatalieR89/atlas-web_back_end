d135b91c88a6eca88074e0fd5e59f04ecd764d6f
NatalieR89 <natalieritch89@gmail.com>
2025-03-12 20:47:41 -0500

    lint fixes

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,15 +1,14 @@
-// 5-typed_arrays.js
 function createInt8TypedArray(length, position, value) {
-if (position >= length || position < 0) {
-throw new Error('Position outside range');
-}
+  if (position >= length || position < 0) {
+    throw new Error('Position outside range');
+  }
 
-const buffer = new ArrayBuffer(length);
-const dataView = new DataView(buffer);
+  const buffer = new ArrayBuffer(length);
+  const dataView = new DataView(buffer);
 
-dataView.setInt8(position, value);
+  dataView.setInt8(position, value);
 
-return dataView;
-}
+  return dataView;
+  }
 
 export default createInt8TypedArray;
