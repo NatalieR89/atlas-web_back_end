ea869445daa20a581fabd1fff1d35bb9df1be313
NatalieR89 <natalieritch89@gmail.com>
2025-03-12 21:16:44 -0500

    changed let to const

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -3,7 +3,7 @@ function updateUniqueItems(groceriesMap) {
     throw new Error('Cannot process');
   }
 
-  for (let [key, value] of groceriesMap) {
+  for (const [key, value] of groceriesMap) {
     if (value === 1) {
       groceriesMap.set(key, 100);
     }
