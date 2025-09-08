226690a387fb0fe485e2e6ac618e5eee18bbd926
NatalieR89 <natalieritch89@gmail.com>
2025-09-08 03:10:45 -0500

    pycode line length fix

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -5,5 +5,5 @@ from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """Return a tuple where first element is k and second is v squared as a float"""
+    """Return a tuple where first element is k and second is v"""
     return (k, float(v ** 2))
