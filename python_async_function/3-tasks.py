c820d804c5c70f15d3ec820be1c5dbb24096bf8a
NatalieR89 <natalieritch89@gmail.com>
2025-09-09 20:40:16 -0500

    docu fix

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -8,6 +8,7 @@ from importlib import import_module
 module = import_module("0-basic_async_syntax")
 wait_random = module.wait_random
 
+
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """
     Returns an asyncio.Task object that wraps the wait_random coroutine
