5b01052c16de1f626fc5af9d7c07fd1d006ced80
NatalieR89 <natalieritch89@gmail.com>
2025-09-10 17:46:13 -0500

    pycode fixes

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -5,10 +5,11 @@ from typing import Union
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
+
 async def measure_runtime() -> float:
     """
-    Executes async_comprehension four times in parallel using asyncio.gather(*...)
-    and returns the total runtime in seconds.
+    Executes async_comprehension in parallel using asyncio.gather
+    and returns runtime in seconds.
     """
     start_time = time.perf_counter()
     await asyncio.gather(*[async_comprehension() for _ in range(4)])
