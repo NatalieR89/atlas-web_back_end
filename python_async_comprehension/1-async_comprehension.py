0960f75855abfb876591a22e26e8ac0d9506b252
NatalieR89 <natalieritch89@gmail.com>
2025-09-10 17:42:27 -0500

    added doumentation

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+"""
+Module 1-async_comprehension
+Contains the coroutine async_comprehension that collects 10 random
+numbers from async_generator using async comprehension.
+"""
+
 from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
