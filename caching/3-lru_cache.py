14c0020a4518a94037ecee7a818f93382ed381e2
NatalieR89 <natalieritch89@gmail.com>
2025-09-21 17:11:39 -0500

    3 lru

---
+++ caching/3-lru_cache.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" LRUCache module
+"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class LRUCache(BaseCaching):
+    """ LRUCache defines a LRU caching system """
+
+    def __init__(self):
+        """ Initialize the cache """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """ Add an item in the cache (LRU algorithm) """
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key)
+            self.cache_data[key] = item
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            lru_key, _ = self.cache_data.popitem(last=False)
+            print(f"DISCARD: {lru_key}")
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key """
+        if key is None or key not in self.cache_data:
+            return None
+
+        self.cache_data.move_to_end(key)
+        return self.cache_data[key]
