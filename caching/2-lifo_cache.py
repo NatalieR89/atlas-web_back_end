aadf778f303573f9c9ea01d0c8e94847730a0dba
NatalieR89 <natalieritch89@gmail.com>
2025-09-21 17:06:52 -0500

    2 lifo

---
+++ caching/2-lifo_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" LIFOCache module
+"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ LIFOCache defines a LIFO caching system """
+
+    def __init__(self):
+        """ Initialize the cache """
+        super().__init__()
+        self.last_key = None
+
+    def put(self, key, item):
+        """ Add an item in the cache (LIFO) """
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            self.last_key = key
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            if self.last_key is not None:
+                del self.cache_data[self.last_key]
+                print(f"DISCARD: {self.last_key}")
+
+        self.cache_data[key] = item
+        self.last_key = key
+
+    def get(self, key):
+        """ Get an item by key """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
